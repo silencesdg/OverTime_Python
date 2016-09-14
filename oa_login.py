e484e7a045d477ba68e0e9df9026105c490895fd
silencesdg <silencesdg@163.com>
2016-09-14 18:34:20 +0800

    修改请求

--- oa_login.py
+++ oa_login.py
@@ -18,7 +18,7 @@ class user:
 
 class poster:
     url = "http://moa.17u.cn"
-    cookie = {}
+    cookie = ""
     header = [("Accept-Encoding", "gzip,deflate"), ("Accept-Language", "zh-CN,en-US;q=0.8"), (
         "User-Agent",
         "Mozilla/5.0 (Linux; Android 5.1; m1 note Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile MQQBrowser/6.2 TBS/036558 Safari/537.36 MicroMessenger/6.3.25.861 NetType/WIFI Language/zh_CN"),
@@ -36,37 +36,36 @@ class poster:
 
         return r.read()
 
-def start(u:user):
-
-    p = poster()
-
-
-    # 验证马
+def veri(p:poster,u:user):
     print('\n获取验证码中。。。')
     p.data = [("mod", 'GetverificationCode'), ("employeeCode", u.code), ("employeePwd", u.psw), ("____v", random.random())]
     rCheck = p.excute().decode('utf-8')
-    print(rCheck)
+    strRep = json.loads(rCheck)
+    repHeader = strRep['responseHeader']
+    strResult = repHeader['result']
+    if strResult == "yes":
+        print('\n获取成功')
+    else:
+        print('\n用户名或密码错误')
+        exit()
 
 
-
-    # 登录
+def login(p:poster,u:user):
     checkCode = input("\n收到的验证码是:")
     print('\n正在登录中。。。')
     p.data = [("mod", 'onLogin'), ("employeeCode", u.code), ("employeePwd", u.psw),("checkcode",checkCode), ("____v", random.random())]
     rLogin = p.excute().decode('utf-8')
-    print(rLogin)
     strRep = json.loads(rLogin)
     repHeader = strRep['responseHeader']
     strResult = repHeader['result']
     if strResult == "ok":
+        #TODO:设置cookie
         print('\n登陆成功')
     else:
         print('\n验证码不正确或已过期')
-        return
+        exit()
 
-
-
-    # 拉取加班数据
+def fetchData(p:poster):
     print('\n正在拉取加班数据。。。')
     p.data = [("mod", "onLoad"), ("pageIndex", "1"), ("StartTime", ""), ("EndTime",""), ("____v", random.random())]
     rOverTimes = p.excute().decode('utf-8')
@@ -80,9 +79,24 @@ def start(u:user):
     print(s)
 
 
+def start(u:user):
+
+    p = poster()
+
+    # 验证马
+    veri(p,u)
+
+    # 登录
+    login(p,u)
+
+    # 拉取加班数据
+    fetchData(p)
+
+
+
 if __name__ == '__main__':
-    code = ""
-    psw = ""
+    code = "12732"
+    psw = "SKL12732"
 
     u = user(code,psw)
     start(u)
