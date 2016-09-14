4fa42cdee3d4175c25719acf139f174e133638a1
宋坤灵12732 <skl12732@LY.com>
2016-09-14 09:23:52 +0800

    提交代码-添加user类

--- oa_login.py
+++ oa_login.py
@@ -1,22 +1,41 @@
 import urllib.request
 import urllib.parse
 import http.cookiejar
+import random
+import json
 
-def post(code,psw):
-	url = "http://moa.17u.cn"
-	data = [("mod","GetverificationCode"),("employeeCode",code),("employeePwd",psw),("____v","0.3410145636950876")]
-	post_date = urllib.parse.urlencode(data).encode('utf-8')
-	# header = {"Accept-Encoding": "gzip,deflate","Accept-Language": "zh-CN,en-US;q=0.8","User-Agent": "Mozilla/5.0 (Linux; Android 5.1; m1 note Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile MQQBrowser/6.2 TBS/036558 Safari/537.36 MicroMessenger/6.3.25.861 NetType/WIFI Language/zh_CN","X-Requested-With": "com.tencent.mm"}
-	cookie = http.cookiejar.CookieJar()
-	openner = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
-	openner.addheaders = [("Accept-Encoding", "gzip,deflate"),("Accept-Language", "zh-CN,en-US;q=0.8"),("User-Agent","Mozilla/5.0 (Linux; Android 5.1; m1 note Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile MQQBrowser/6.2 TBS/036558 Safari/537.36 MicroMessenger/6.3.25.861 NetType/WIFI Language/zh_CN"),("X-Requested-With","com.tencent.mm")]
-	r = openner.open(url,post_date)
-	print(r.read().decode('utf-8'))
+class user:
+    code = ''
+    psw = ''
+    def __init__(self,code,psw):
+        self.code = code
+        self.psw = psw
 
-	# req = urllib.request.Request(url,post_date,header)
-	# rep = urllib.request.urlopen(req)
-	# print(rep)
+def post(data):
+    url = ""
+
+    post_date = urllib.parse.urlencode(data).encode('utf-8')
+    cookie = http.cookiejar.CookieJar()
+    openner = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+    openner.addheaders = [("Accept-Encoding", "gzip,deflate"),("Accept-Language", "zh-CN,en-US;q=0.8"),("User-Agent","Mozilla/5.0 (Linux; Android 5.1; m1 note Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile MQQBrowser/6.2 TBS/036558 Safari/537.36 MicroMessenger/6.3.25.861 NetType/WIFI Language/zh_CN"),("X-Requested-With","com.tencent.mm")]
+    r = openner.open(url,post_date)
+
+    print(r.read().decode('utf-8'))
+
+def veri(u:user):
+    dataVeri = [("mod", 'GetverificationCode'), ("employeeCode", u.code), ("employeePwd", u.psw), ("____v", random.random())]
+    post(dataVeri)
+
+def login(u:user,checkCode):
+    dataLogin = [("mod", 'onLogin'), ("employeeCode", u.code), ("employeePwd", u.psw),("checkcode",checkCode), ("____v", random.random())]
+    post(dataLogin)
 
 
 if __name__ == '__main__':
-	post("","")
+    code = ""
+    psw = ""
+
+    u = user(code,psw)
+
+ #   veri(u)
+    login(u,'')
