d265ca15be43c3356db84ac53b40e07610d8d0bc
silencesdg <silencesdg@163.com>
2016-09-14 16:13:11 +0800

    完善请求流程

--- oa_login.py
+++ oa_login.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
+# encoding: UTF-8
+
 import urllib.request
 import urllib.parse
 import http.cookiejar
 import random
+import re
 import json
 
 class user:
@@ -11,24 +15,69 @@ class user:
         self.code = code
         self.psw = psw
 
-def post(data):
-    url = ""
 
-    post_date = urllib.parse.urlencode(data).encode('utf-8')
-    cookie = http.cookiejar.CookieJar()
-    openner = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
-    openner.addheaders = [("Accept-Encoding", "gzip,deflate"),("Accept-Language", "zh-CN,en-US;q=0.8"),("User-Agent","Mozilla/5.0 (Linux; Android 5.1; m1 note Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile MQQBrowser/6.2 TBS/036558 Safari/537.36 MicroMessenger/6.3.25.861 NetType/WIFI Language/zh_CN"),("X-Requested-With","com.tencent.mm")]
-    r = openner.open(url,post_date)
+class poster:
+    url = "http://moa.17u.cn"
+    cookie = {}
+    header = [("Accept-Encoding", "gzip,deflate"), ("Accept-Language", "zh-CN,en-US;q=0.8"), (
+        "User-Agent",
+        "Mozilla/5.0 (Linux; Android 5.1; m1 note Build/LMY47D) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile MQQBrowser/6.2 TBS/036558 Safari/537.36 MicroMessenger/6.3.25.861 NetType/WIFI Language/zh_CN"),
+                              ("X-Requested-With", "com.tencent.mm")]
+    data = []
+    def excute(self,):
+        assert self.data,'poster.data cannot be empty'
+        post_date = urllib.parse.urlencode(self.data).encode('utf-8')
+        if not self.cookie:
+            self.cookie = http.cookiejar.CookieJar()
 
-    print(r.read().decode('utf-8'))
+        openner = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(self.cookie))
+        openner.addheaders = self.header
+        r = openner.open(self.url, post_date)
 
-def veri(u:user):
-    dataVeri = [("mod", 'GetverificationCode'), ("employeeCode", u.code), ("employeePwd", u.psw), ("____v", random.random())]
-    post(dataVeri)
+        return r.read()
 
-def login(u:user,checkCode):
-    dataLogin = [("mod", 'onLogin'), ("employeeCode", u.code), ("employeePwd", u.psw),("checkcode",checkCode), ("____v", random.random())]
-    post(dataLogin)
+def start(u:user):
+
+    p = poster()
+
+
+    # 验证马
+    print('\n获取验证码中。。。')
+    p.data = [("mod", 'GetverificationCode'), ("employeeCode", u.code), ("employeePwd", u.psw), ("____v", random.random())]
+    rCheck = p.excute().decode('utf-8')
+    print(rCheck)
+
+
+
+    # 登录
+    checkCode = input("\n收到的验证码是:")
+    print('\n正在登录中。。。')
+    p.data = [("mod", 'onLogin'), ("employeeCode", u.code), ("employeePwd", u.psw),("checkcode",checkCode), ("____v", random.random())]
+    rLogin = p.excute().decode('utf-8')
+    print(rLogin)
+    strRep = json.loads(rLogin)
+    repHeader = strRep['responseHeader']
+    strResult = repHeader['result']
+    if strResult == "ok":
+        print('\n登陆成功')
+    else:
+        print('\n验证码不正确或已过期')
+        return
+
+
+
+    # 拉取加班数据
+    print('\n正在拉取加班数据。。。')
+    p.data = [("mod", "onLoad"), ("pageIndex", "1"), ("StartTime", ""), ("EndTime",""), ("____v", random.random())]
+    rOverTimes = p.excute().decode('utf-8')
+    print('\n拉取加班数据成功')
+    print('\n')
+    print(rOverTimes)
+
+    #过滤数据
+    pattern = re.compile(r'<tr><td>[*]</td><td>[*]</td></tr>')
+    s = pattern.findall(rOverTimes)
+    print(s)
 
 
 if __name__ == '__main__':
@@ -36,6 +85,4 @@ if __name__ == '__main__':
     psw = ""
 
     u = user(code,psw)
-
- #   veri(u)
-    login(u,'')
+    start(u)
